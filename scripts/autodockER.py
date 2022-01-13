295326676b3d5129ccf6f8ae2c14429db4188657
Noah Burget <51685498+Nuwah12@users.noreply.github.com>
2022-01-13 16:38:23 -0500

    Add files via upload

---
+++ scripts/autodockER.py
@@ -0,0 +1,102 @@
+
+"""
+This script is setup to be run with a specific directory structure:
+
+DISCO
+|Gene
+ |PDB (folder containing ligand-receptor complex files)
+
+For each ligand-receptor pair, docking is performed and RMSD calculated from original crystal strucutre given in PDB folder
+Smina calls (lines 49-57) have specific information on parameters and such
+"""
+
+import os
+from pymol import cmd
+
+def main():
+	ligand_file = ""
+
+	os.chdir("/media/noah/1E7A-DEB2/DISCO")
+
+	files = os.listdir()
+
+	for f in files:
+		os.chdir("/media/noah/1E7A-DEB2/DISCO/" + f) 
+
+		print("CURR DIR:" + os.getcwd())
+
+		for fi in os.listdir():
+			if "Cros" in fi:
+				continue
+			os.chdir("/media/noah/1E7A-DEB2/DISCO/" + f + "/" + fi)
+
+			# Simple iterative search for ligand/target files
+			for g in os.listdir(): 
+				print(g)
+				if "_LIG" in g or "_LIG0" in g:
+					ligand_file = g
+					print("ligand file = {}".format(ligand_file))
+				elif "_PRO" in g:
+					target_file = g
+					print("target file = {}".format(target_file))
+				else:
+					continue
+
+			try:
+
+				cmd.load(ligand_file)
+				cmd.load(target_file)
+				center_coords = cmd.centerofmass(ligand_file[:-4])
+			except:
+				print("\nProblems determining center of mass\n")
+				continue
+
+			print("\nDocking {} to {}".format(ligand_file, target_file))
+
+			# smina calls
+			os.system("""/home/noah/Desktop/smina/smina.static -r {receptor} -l {ligand} --center_x {x_coord} --center_y {y_coord} --center_z {z_coord} --autobox_ligand {ligand} --exhaustiveness 8 --num_modes 9 --atom_terms {termfile_name} -o {outfile_name} --custom_scoring {scoring_file}
+				""".format(receptor = target_file, ligand = ligand_file, 
+					x_coord = center_coords[0], y_coord = center_coords[1],
+					z_coord = center_coords[2], termfile_name = ligand_file[:4] + "_terms.at",
+					outfile_name = ligand_file[:4] + "_out.pdb", scoring_file = "/home/noah/customscore.score"))
+
+			os.system("""/home/noah/Desktop/smina/smina.static -r {receptor} -l {ligand} --center_x {x_coord} --center_y {y_coord} --center_z {z_coord} --autobox_ligand {ligand} --minimize_iters 1 --local_only -o {outfile_name} --custom_scoring {scoring_file}""".format(receptor = target_file, ligand = ligand_file, 
+					x_coord = center_coords[0], y_coord = center_coords[1],
+					z_coord = center_coords[2], outfile_name = ligand_file[:4] + "_rnamed.pdb", scoring_file  = "/home/noah/customscore.score"))
+
+			dat = ""
+
+			# Concatenation of unchanged crystal structure with docking resulting .pdb file
+			try:
+				with open(ligand_file[:4] + "_rnamed.pdb") as rnamed_file:
+					dat = rnamed_file.read()
+
+				dat += "\n"
+
+				with open(ligand_file[:4] + "_out.pdb") as out:
+					og_dat = out.read()
+
+				dat += og_dat
+
+				with open(ligand_file[:4] + "_FINAL.pdb", "w") as file:
+					file.write(dat)
+
+				# RMSD calculations using intra_fit()
+				cmd.load(ligand_file[:4] + "_FINAL.pdb")
+				rmsds = cmd.intra_fit(ligand_file[:4] + "_FINAL")
+
+				i = 1
+				with open("rmsd_calculations.txt", "w") as out:
+					for r in rmsds:
+						out.write("Pose " + str(i) + ": " + str(r) + "\n")
+						i+=1
+			except:
+				continue		
+			cmd.delete(ligand_file)
+			cmd.delete(target_file)
+
+		#os.chdir("/home/noah/Desktop/refined-set")
+	
+
+if __name__ == '__main__':
+	main()	
---
+++ scripts/scraper.py
@@ -0,0 +1,103 @@
+import os
+import pandas as pd
+
+DEFAULT_DIR = "/home/noah/Desktop/refined-set"
+def main():
+	termfile = None
+	rmsdfile = None
+	master = []
+	inner = []
+	aff = None
+
+	os.chdir(DEFAULT_DIR)
+	for f in os.listdir():
+		os.chdir(DEFAULT_DIR + "/" + f)
+		files = findCriticalFiles()
+		if files[3] == False:
+			continue
+
+		with open(files[1], "r") as rmsds:
+			linecount = 1
+			for line in rmsds:
+				line = line.split()
+				if float(line[2]) <= 1.5 and float(line[2]) != -1.0:
+					aff = getAffinity(linecount, files[2])
+					print("Affinity:", aff)
+					inner.append(aff)
+					inner.append(f)
+					inner.append(float(line[2]))
+					for g in getTerms(files[0], linecount):
+						inner.append(g)
+				linecount += 1
+				master.append(inner)
+				inner = []
+				#print(master)
+	toCSV(master)
+
+def getTerms(termfile, pose):
+	pose_count = 1
+	gauss = []
+	posgauss= []
+	repulsion = []
+	hydrophobic = []
+	hbond = []
+	comp = []
+
+	with open(termfile, "r") as terms:
+		for line in terms:
+			if "END" in line:
+				pose_count += 1
+			elif pose_count == pose and "atomid" not in line:
+				line = line.split()
+				gauss.append(line[1])
+				posgauss.append(line[2])
+				repulsion.append(line[3])
+				hydrophobic.append(line[4])
+				hbond.append(line[5])
+			elif pose_count > pose:
+				break
+	comp.append(gauss)
+	comp.append(posgauss)
+	comp.append(repulsion)
+	comp.append(hydrophobic)
+	comp.append(hbond)
+
+	return comp
+
+def findCriticalFiles():
+	termfile = None
+	rmsdfile = None
+	outfile = None
+	for l in os.listdir():
+		if "_terms" in l:
+			print("termfile: ", termfile)
+			termfile = l
+		elif "rmsd" in l:
+			rmsdfile = l
+			print("rmsdfile: ", rmsdfile)
+		elif "_out" in l:
+			outfile = l
+	if termfile != None and rmsdfile != None:
+		return [termfile, rmsdfile, outfile, True]
+	else:
+		return [termfile, rmsdfile, outfile, False]
+
+def getAffinity(pose, inf):
+	currpose = 1
+	print("Outfile for affinity:", inf)
+	with open(inf, "r") as infile:
+		for line in infile:
+			if "ENDMDL" in line:
+				currpose+=1
+			elif "minimizedAffinity" in line and currpose == pose:
+				line = line.split()
+				return line[2]
+			
+
+def toCSV(arr):
+	os.chdir(DEFAULT_DIR)
+	df = pd.DataFrame(arr)
+	df.to_csv("refined-set-terms.csv")
+
+if __name__ == '__main__':
+	main()
